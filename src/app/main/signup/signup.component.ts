f6b462ef6f82b0d2b201ebc525458009a348e60a
neurek88 <neurek88@gmail.com>
2020-10-10 15:56:06 -0500

    added photo gallery.

--- src/app/main/signup/signup.component.ts
+++ src/app/main/signup/signup.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-    test : Date = new Date();
+    test: Date = new Date();
     focus;
     focus1;
     images = [
@@ -16,7 +16,20 @@ export class SignupComponent implements OnInit {
         "./assets/img/Volunteer/IMG_2624.JPG",
         "./assets/img/Volunteer/IMG_2688.JPG",
         "./assets/img/Volunteer/IMG_2690.PNG",
-        "./assets/img/Volunteer/IMG_5914.JPG"
+        "./assets/img/Volunteer/IMG_5914.JPG",
+        "./assets/img/Volunteer/Morgan.JPG",
+        "./assets/img/Volunteer/Morgan2.JPG"
+    ]
+
+    personal = [
+        "./assets/img/Personal/File_001.png",
+        "./assets/img/Personal/IMG_0081.jpg",
+        "./assets/img/Personal/IMG_1733.JPG",
+        "./assets/img/Personal/IMG_1736.JPG",
+        "./assets/img/Personal/IMG_2734.jpg",
+        "./assets/img/Personal/IMG_2805.jpg",
+        "./assets/img/Personal/IMG_2818.jpg"
+
         ]
 
     constructor() { }
